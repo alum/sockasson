9dfae2d6c8ed1ae4f850d7956f8576a28868df84
Filip Bonnevier <filip.bonnevier@gmail.com>
2017-06-18 23:21:45 +0300

    remove unused line

--- lstm_text_generation.py
+++ lstm_text_generation.py
@@ -20,7 +20,6 @@ import numpy as np
 import random
 import sys
 
-#path = get_file('nietzsche.txt', origin='https://s3.amazonaws.com/text-datasets/nietzsche.txt')
 path = './blob-cornelis'
 text = open(path).read().lower()
 print('corpus length:', len(text))
